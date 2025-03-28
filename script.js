bb88bd93fffca236ad3d5986f6c0566169d92f06
wikoq <wiktorro.mor2@gmail.com>
2025-03-28 23:59:30 +0100

    big changes

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("sperma")
+const playStopBtn = document.getElementById("play-stop-btn")
+
+playStopBtn.addEventListener("click", () => {
+    if(playStopBtn.classList.contains("play-btn")){
+        playStopBtn.classList.add("stop-btn")
+        playStopBtn.classList.remove("play-btn")
+    }else{
+        playStopBtn.classList.remove("stop-btn")
+        playStopBtn.classList.add("play-btn")
+    }
+})
