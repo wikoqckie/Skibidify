3fc9ea27215edc89c1d933fe2e8739aacac0db54
wikoq <wiktorro.mor2@gmail.com>
2025-03-29 15:10:14 +0100

    big photos

--- script.js
+++ script.js
@@ -1,4 +1,69 @@
 const playStopBtn = document.getElementById("play-stop-btn")
+const infoArea = document.getElementById("info-area")
+
+const songName = document.getElementById("song-name")
+const bigFoto = document.getElementById("big-photo")
+
+// ARTISTS BUTTONS ID
+
+const fagata = document.querySelector(".fagata")
+const palion = document.querySelector(".palion")
+const genzie = document.querySelector(".genzie")
+const nirvana = document.querySelector(".nirvana")
+const mfdoom = document.querySelector(".mfdoom")
+const travisscott = document.querySelector(".travisscott")
+
+// ARTISTS BUTTONS FUNCTIONALITY
+
+function infoFull(){
+    bigFoto.classList.remove("fagata-big")
+    bigFoto.classList.remove("palion-big")
+    bigFoto.classList.remove("genzie-big")
+    bigFoto.classList.remove("nirvana-big")
+    bigFoto.classList.remove("mfdoom-big")
+    bigFoto.classList.remove("travisscott-big")
+
+    infoArea.classList.add("full-info")
+    infoArea.classList.remove("empty-info")
+}
+
+fagata.addEventListener("click", () => {
+    infoFull()
+    songName.innerText = "Fagata X Lil Gnar - Kubki"
+    bigFoto.classList.add("fagata-big")
+})
+
+palion.addEventListener("click", () => {
+    infoFull()
+    songName.innerText = "♪ PALION - ZIELONE feat. NeoN ♪"
+    bigFoto.classList.add("palion-big")
+})
+
+genzie.addEventListener("click", () => {
+    infoFull()
+    songName.innerText = "GENZIE - GENZIARA"
+    bigFoto.classList.add("genzie-big")
+})
+
+nirvana.addEventListener("click", () => {
+    infoFull()
+    songName.innerText = "Nirvana - Come As You Are"
+    bigFoto.classList.add("nirvana-big")
+})
+
+mfdoom.addEventListener("click", () => {
+    infoFull()
+    songName.innerText = "MF DOOM - One Beer"
+    bigFoto.classList.add("mfdoom-big")
+})
+
+travisscott.addEventListener("click", () => {
+    infoFull()
+    songName.innerText = "Travis Scott - FE!N (ft. Playboi Carti)"
+    bigFoto.classList.add("travisscott-big")
+})
+
+// PLAY & STOP BTN
 
 playStopBtn.addEventListener("click", () => {
     if(playStopBtn.classList.contains("play-btn")){
